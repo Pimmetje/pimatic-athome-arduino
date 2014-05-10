489e26b0acdb0e1669765662025a76c745c1742b
Dirk Fischbach <dirk@fischbachnet.de>
2014-05-10 21:39:05 +0200

    Removed old header.

--- AtHome/AtHomeReceiver/AtHome.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef AtHome_h
-#define AtHome_h
-
-#define atHomeReceiverNodeID 	1               // RF12 node ID in the range 1-30
-#define atHomeNetworkGroup      210             // RF12 Network group
-#define atHomeFreq              RF12_868MHZ 	// Frequency of RFM12B module
-
-#endif
